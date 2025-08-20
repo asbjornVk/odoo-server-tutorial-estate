0662e4e8443be0245bbdc5f2ec74e79839571caa
Asbjørn Jacobsen <asbjorn@vkdata.dk>
2025-08-20 08:46:54 +0200

    [ADD] yesterdays product, comittet this morning.

--- tutorials/awesome_clicker/static/src/click_rewards.js
+++ tutorials/awesome_clicker/static/src/click_rewards.js
@@ -2,16 +2,17 @@ export const rewards = [
     {
         description: "Get 1 click bot",
         apply(clicker) {
-            clicker.increment(1);
+            clicker.bots.clickbot.purchased += 1;
+            
         },
         maxLevel: 3,
     },
     {
         description: "Get 10 click bot",
         apply(clicker) {
-            clicker.increment(10);
+            clicker.bots.clickbot.purchased += 10;
         },
-        minLevel: 3,
+        minLevel: 1,
         maxLevel: 4,
     },
     {
--- tutorials/awesome_clicker/static/src/clicker_model.js
+++ tutorials/awesome_clicker/static/src/clicker_model.js
@@ -9,22 +9,40 @@ export class ClickerModel extends Reactive {
         this.clicks = 0;
         this.level = 0;
         this.bus = new EventBus();
-
         this.bots = {
             clickbot: {
-                price: 100,
-                level: 1,
+                price: 10,
+                level: 0,
                 increment: 10,
                 purchased: 0,
             },
             bigbot: {
-                price: 500,
-                level: 2,
+                price: 50,
+                level: 1,
                 increment: 100,
                 purchased: 0,
             }
         };
-        this.multiplier = 1;
+        this.trees = {
+            pearTree: {
+                price: 1000,
+                level: 4,
+                produce: "pear",
+                purchased: 0,
+            },
+            cherryTree: {
+                price: 1000,
+                level: 4,
+                produce: "cherry",
+                purchased: 0,
+            },
+        }
+        this.fruits = {
+            pear: 0,
+            cherry: 0,
+        },
+        this.multiplier = 1
+        this.ticks = 0;
     }
 
     addClick() {
@@ -32,31 +50,37 @@ export class ClickerModel extends Reactive {
     }
 
     tick() {
+        this.ticks++;
         for (const bot in this.bots) {
             this.clicks += this.bots[bot].increment * this.bots[bot].purchased * this.multiplier;
+        }
+        if (this.ticks % 3 === 0) {
+            for (const tree in this.trees) {
+                this.fruits[this.trees[tree].produce] += this.trees[tree].purchased;
+            }
         }
     }
 
     buyMultiplier() {
-        if (this.clicks < 5000) {
+        if (this.clicks < 100) {
             return false;
         }
-        this.clicks -= 5000;
+        this.clicks -= 100;
         this.multiplier++;
     }
 
     increment(inc) {
         this.clicks += inc;
-        if (this.milestones[this.level] &&
-            this.clicks >= this.milestones[this.level].clicks) {
-            this.bus.trigger("MILESTONE", this.milestones[this.level]);
-            this.level += 1;
-        }
+        if (this.milestones[this.level] && this.clicks >= this.milestones[this.level].clicks) 
+            {
+                this.bus.trigger("MILESTONE", this.milestones[this.level]);
+                this.level += 1;
+            }
     }
 
     buyBot(name) {
         if (!Object.keys(this.bots).includes(name)) {
-            throw new Error('Invalid bot name ${name}');
+            throw new Error(`Invalid bot name ${name}`);
         }
         if (this.clicks < this.bots[name].price) {
             return false;
@@ -67,21 +91,38 @@ export class ClickerModel extends Reactive {
 
     giveReward() {
         const availableReward = [];
-        for(const reward of rewards){
-            if (reward.minLevel <= this.level || !reward.minLevel && reward.maxLevel >= this.level || !reward.maxLevel){
+        for (const reward of rewards) {
+            if (reward.minLevel <= this.level || !reward.minLevel) {
+                if (reward.maxLevel >= this.level || !reward.maxLevel) {
                     availableReward.push(reward);
                 }
             }
-        
+        }
+        const reward = choose(availableReward);
+        this.bus.trigger("REWARD", reward);
         return choose(availableReward);
     }
+    
+    buyTree(name) {
+        if (!Object.keys(this.trees).includes(name)) {
+            throw new Error(`Invalid tree name ${name}`);
+        }
+        if (this.clicks < this.trees[name].price) {
+            return false;
+        }
+        this.clicks -= this.trees[name].price;
+        this.trees[name].purchased += 1;
+    }
+
 
     get milestones() {
-        return [
-            { clicks: 100, unlock: "clickBot" },
-            { clicks: 500, unlock: "bigBot" },
-            { clicks: 5000, unlock: "power multiplier" },
-        ];
-    }
-}
+            return [
+                { clicks: 10, unlock: "clickBot" },
+                { clicks: 50, unlock: "bigBot" },
+                { clicks: 100, unlock: "power multiplier" },
+                { clicks: 1000, unlock: "pear tree & cherry tree" },
 
+
+            ];
+        }
+    }
